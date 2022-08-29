5851b3799524e60ec6584eab211e16cad682b54d
Evelin <ladyblue@freemail.hu>
2022-08-29 16:43:21 +0200

    solution

--- 09-storage/exercises/01/solution/js/setCookie.js
+++ 09-storage/exercises/01/solution/js/setCookie.js
@@ -1,1 +1,7 @@
-export default setCookie;
+const setCookie = (value) => {
+    const expires = new Date(Date.now() + (15 * 60 * 1000));
+    document.cookie = `token=${value}; expires=${expires.toUTCString()}`;
+  };
+  
+  export default setCookie;
+  
